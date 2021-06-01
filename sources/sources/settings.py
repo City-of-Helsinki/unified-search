85af864f949abf177f6c2e914011368b19a5d9f3
Mika Kaukoranta <mika.kaukoranta@mavericks.fi>
2021-06-01 15:15:24 +0300

    Fetch only current events (#29)
    * Fetch only current events
    
    * Use prod linkedevents for events index

--- sources/sources/settings.py
+++ sources/sources/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ES_URI = os.getenv("ES_URI")
 
 # Allow custom URL for testing purposes e.g. to match dev version of Linkedevents
-EVENT_URL = os.getenv("EVENT_URL", "https://api.hel.fi/linkedevents/v1/event/")
+EVENT_URL = os.getenv("EVENT_URL", "https://api.hel.fi/linkedevents/v1/event/?start=today")
 
 DEBUG = os.getenv("DEBUG", "false").lower() in ("yes", "true", "t", "1")
 
