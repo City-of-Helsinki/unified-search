12c7d52c37c7d2d133d93f77727608d5f6ba6631
Kari Salminen <kari.salminen@anders.com>
2023-10-23 16:03:41 +0300

    refactor: refactor Accessibility.fix_unknown_and_zero_shortcomings
    Deferred refactorings from PR #107
    https://github.com/City-of-Helsinki/unified-search/pull/107
    are contained in this commit.
    
    refs US-115

--- sources/ingest/importers/location.py
+++ sources/ingest/importers/location.py
@@ -260,6 +260,31 @@ class AccessibilitySentence:
     sentence: LanguageString
 
 
+def unique_shortages_count_for_viewpoint_ids(
+    viewpoint_ids: List[str],
+    viewpoint_id_to_shortages: Dict[str, List[LanguageString]],
+):
+    """
+    Count unique accessibility shortages for given accessibility viewpoint IDs.
+
+    @param viewpoint_ids A list of AccessibilityViewpointID values
+    @param viewpoint_id_to_shortages A mapping from AccessibilityViewpointID values to
+                                     their accessibility shortages
+    @note Omits known accessibility shortages that mean "no accessibility shortcomings"
+          in the calculation.
+    @return The number of unique accessibility shortages for the given accessibility
+            viewpoint IDs
+    """
+    return len(
+        set(
+            shortage
+            for viewpoint_id in viewpoint_ids
+            for shortage in viewpoint_id_to_shortages.get(viewpoint_id, [])
+        )
+        - set(NO_SHORTCOMINGS_SHORTAGE_TEXT_VARIANTS)
+    )
+
+
 @dataclass
 class Accessibility:
     email: str
@@ -276,6 +301,79 @@ class Accessibility:
         for viewpoint in self.viewpoints:
             viewpoint.shortages = viewpoint_id_to_shortages.get(viewpoint.id, [])[:]
 
+    def viewpoint_id_to_shortages(self) -> Dict[str, List[LanguageString]]:
+        """
+        Mapping from AccessibilityViewpointID values to their accessibility shortages
+
+        @warning This method should only be called after set_accessibility_shortages()
+                 has been called.
+        @raises ValueError If any viewpoints' shortages have not been set i.e.
+                           any(vp.shortages is None for vp in self.viewpoints)
+        @return A mapping from AccessibilityViewpointID values to their accessibility
+                shortages
+        """
+        result: Dict[str, List[LanguageString]] = {
+            viewpoint_id.value: [] for viewpoint_id in AccessibilityViewpointID
+        }
+        for viewpoint in self.viewpoints:
+            if viewpoint.shortages is None:
+                raise ValueError(
+                    "Viewpoint's shortages have not been set, please call "
+                    "self.set_accessibility_shortages before calling this method"
+                )
+            result[viewpoint.id] = viewpoint.shortages
+        return result
+
+    def viewpoint_id_to_value(self) -> Dict[str, str]:
+        """
+        Mapping from AccessibilityViewpointID values to their
+        AccessibilityViewpointValue values
+
+        @return A mapping from AccessibilityViewpointID values to their
+                AccessibilityViewpointValue values
+        """
+        result: Dict[str, str] = {
+            viewpoint_id.value: AccessibilityViewpointValue.UNKNOWN.value
+            for viewpoint_id in AccessibilityViewpointID
+        }
+        for viewpoint in self.viewpoints:
+            result[viewpoint.id] = viewpoint.value
+        return result
+
+    def profile_shortcomings(self) -> Dict[str, Optional[int]]:
+        """
+        Mapping from all possible AccessibilityProfile values to their accessibility
+        shortcomings counts, or to null if unknown
+
+        @return A mapping from all possible AccessibilityProfile values to their
+                accessibility shortcomings counts, or to null if unknown
+        """
+        result: Dict[str, Optional[int]] = {
+            profile.value: None for profile in AccessibilityProfile
+        }
+        for shortcoming in self.shortcomings:
+            result[shortcoming.profile] = shortcoming.count
+        return result
+
+    def update_shortcomings(
+        self, profile_shortcomings: Dict[str, Optional[int]]
+    ) -> bool:
+        """
+        Update self.shortcomings using given mapping from AccessibilityProfile values to
+        their optional shortcoming counts.
+
+        @return True if self.shortcomings contents got changed, False otherwise.
+        """
+        fixed_shortcomings = [
+            AccessibilityShortcoming(profile=profile, count=count)
+            for profile, count in profile_shortcomings.items()
+        ]
+        has_changed = len(self.shortcomings) != len(fixed_shortcomings) or any(
+            sc1 != sc2 for sc1, sc2 in zip(self.shortcomings, fixed_shortcomings)
+        )
+        self.shortcomings = fixed_shortcomings
+        return has_changed
+
     def fix_unknown_and_zero_shortcomings(self) -> bool:
         """
         Fix unknown and zero accessibility shortcomings using accessibility viewpoints
@@ -287,65 +385,32 @@ class Accessibility:
                            any(vp.shortages is None for vp in self.viewpoints)
         @return True if any shortcomings were fixed, False otherwise.
         """
-        profile_shortcomings: Dict[str, Optional[int]] = {
-            profile.value: None for profile in AccessibilityProfile
-        }
-        for shortcoming in self.shortcomings:
-            profile_shortcomings[shortcoming.profile] = shortcoming.count
-
-        viewpoint_id_to_shortages: Dict[str, List[LanguageString]] = {
-            viewpoint_id.value: [] for viewpoint_id in AccessibilityViewpointID
-        }
-        for viewpoint in self.viewpoints:
-            if viewpoint.shortages is None:
-                raise ValueError(
-                    "Viewpoint's shortages have not been set, please call "
-                    "self.set_accessibility_shortages before calling this method"
-                )
-            viewpoint_id_to_shortages[viewpoint.id] = viewpoint.shortages
-
-        viewpoint_id_to_value: Dict[str, str] = {
-            viewpoint_id.value: AccessibilityViewpointValue.UNKNOWN.value
-            for viewpoint_id in AccessibilityViewpointID
-        }
-        for viewpoint in self.viewpoints:
-            viewpoint_id_to_value[viewpoint.id] = viewpoint.value
+        profile_shortcomings = self.profile_shortcomings()
+        viewpoint_id_to_shortages = self.viewpoint_id_to_shortages()
+        viewpoint_id_to_value = self.viewpoint_id_to_value()
 
         for profile, viewpoint_ids in ACCESSIBILITY_PROFILE_VIEWPOINTS.items():
-            profile_statuses = {viewpoint_id_to_value[_id] for _id in viewpoint_ids}
+            profile_statuses: Set[str] = {
+                viewpoint_id_to_value[_id] for _id in viewpoint_ids
+            }
             any_reds = AccessibilityViewpointValue.RED.value in profile_statuses
             any_unknowns = AccessibilityViewpointValue.UNKNOWN.value in profile_statuses
             all_greens = profile_statuses == {AccessibilityViewpointValue.GREEN.value}
+
             if profile_shortcomings[profile] in [None, 0]:
                 if any_unknowns:
                     profile_shortcomings[profile] = None  # Mark as unknown
                 elif any_reds:
-                    unique_shortages_count_for_profile = (
-                        len(
-                            set(
-                                shortage
-                                for viewpoint_id in viewpoint_ids
-                                for shortage in viewpoint_id_to_shortages.get(
-                                    viewpoint_id, []
-                                )
-                            )
-                            - set(NO_SHORTCOMINGS_SHORTAGE_TEXT_VARIANTS)
+                    profile_shortcomings[profile] = (
+                        unique_shortages_count_for_viewpoint_ids(
+                            viewpoint_ids, viewpoint_id_to_shortages
                         )
                         or None
                     )  # If "red" but no shortages mark as unknown
-                    profile_shortcomings[profile] = unique_shortages_count_for_profile
                 elif all_greens:
                     profile_shortcomings[profile] = 0
 
-        fixed_shortcomings = [
-            AccessibilityShortcoming(profile=profile, count=count)
-            for profile, count in profile_shortcomings.items()
-        ]
-        has_changed = len(self.shortcomings) != len(fixed_shortcomings) or any(
-            sc1 != sc2 for sc1, sc2 in zip(self.shortcomings, fixed_shortcomings)
-        )
-        self.shortcomings = fixed_shortcomings
-        return has_changed
+        return self.update_shortcomings(profile_shortcomings)
 
 
 class ResourceMainType(Enum):
