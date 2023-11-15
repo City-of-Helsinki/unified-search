bab606cf412331128bf3aa17b26ecde74e5d0f20
Niko Lindroos <niko.j.makela@gmail.com>
2023-11-15 10:55:59 +0200

    refactor(graphql): split datasource code to multiple files
    LIIKUNTA-255.
    The datasource file was too long and hard to understand.
    The types and constants are now moved to new files.
    The require-statements are changed to import-statements.

---
+++ graphql/src/datasources/es/types.ts
@@ -0,0 +1,116 @@
+import { ELASTIC_SEARCH_INDICES, SEARCH_RESULT_FIELDS } from './constants';
+
+export type ElasticSearchIndex = typeof ELASTIC_SEARCH_INDICES[number];
+
+export type SearchResultField = typeof SEARCH_RESULT_FIELDS[number];
+
+export type OntologyTreeParams = {
+  rootId?: string;
+  leavesOnly?: boolean;
+};
+
+export type OntologyWordParams = {
+  ids?: string[];
+};
+
+export type OntologyTreeQuery = {
+  size: number;
+  query?: {
+    bool: OntologyTreeQueryBool;
+  };
+};
+
+export type OntologyTreeQueryBool = {
+  filter?: {
+    bool: {
+      should: [
+        {
+          term: {
+            ancestorIds: string;
+          };
+        },
+        {
+          term: {
+            _id: string;
+          };
+        }
+      ];
+    };
+  };
+  must_not?: {
+    exists: {
+      field: 'childIds';
+    };
+  };
+};
+
+export type AdministrativeDivisionParams = {
+  helsinkiCommonOnly?: boolean;
+};
+
+export type OrderingDirection = 'ASCENDING' | 'DESCENDING';
+
+export type OrderByDistanceParams = {
+  latitude: number;
+  longitude: number;
+  order: OrderingDirection;
+};
+
+export type OrderByNameParams = {
+  order: OrderingDirection;
+};
+
+export type OpenAtFilter = {
+  term: {
+    'venue.openingHours.openRanges': string;
+  };
+};
+
+export type AccessibilityProfileType =
+  | 'hearing_aid'
+  | 'reduced_mobility'
+  | 'rollator'
+  | 'stroller'
+  | 'visually_impaired'
+  | 'wheelchair';
+
+export type TermField =
+  | 'venue.location.administrativeDivisions.id.keyword'
+  | 'links.raw_data.ontologytree_ids_enriched.id'
+  | 'links.raw_data.ontologyword_ids_enriched.id'
+  | 'venue.serviceOwner.providerType.keyword'
+  | 'venue.serviceOwner.type.keyword'
+  | 'venue.targetGroups.keyword';
+
+export type BooleanQueryOccurrenceType =
+  | 'filter'
+  | 'must'
+  | 'must_not'
+  | 'should';
+
+export type ArrayFilter = {
+  bool: {
+    [key in BooleanQueryOccurrenceType]?: Array<{
+      term: {
+        [key: string]: string;
+      };
+    }>;
+  };
+};
+
+export type MustHaveReservableResourceFilter = {
+  bool: {
+    should: [
+      {
+        term: {
+          [key: string]: boolean;
+        };
+      },
+      {
+        exists: {
+          field: string;
+        };
+      }
+    ];
+  };
+};
