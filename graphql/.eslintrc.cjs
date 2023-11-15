cb0de8184122f65c1150d34b93f9e84c15203157
Niko Lindroos <niko.j.makela@gmail.com>
2023-11-15 11:03:47 +0200

    chore(graphql): install and configure eslint and prettier
    LIIKUNTA-255.
    Add the Eslint to check coding style and format the code.
    Used `npm init @eslint/config` to create the configuration.
    Add prettier to help with the code formatting.

---
+++ graphql/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 0,
+    '@typescript-eslint/prefer-nullish-coalescing': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/naming-convention': 0,
+    '@typescript-eslint/semi': 0,
+  },
+  plugins: ['@typescript-eslint'],
+};
