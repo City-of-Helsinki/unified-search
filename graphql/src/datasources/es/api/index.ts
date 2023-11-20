a97561d32cc960a42ca325915ab0b5a79ec32689
Niko Lindroos <niko.j.makela@gmail.com>
2023-11-20 13:39:08 +0200

    refactor(graphql): split es-api request-functions to new files

---
+++ graphql/src/datasources/es/api/index.ts
@@ -0,0 +1,7 @@
+export { default as getAdministrativeDivisions } from './getAdministrativeDivisions';
+export { default as getOntologyTree } from './getOntologyTree';
+export { default as getOntologyWords } from './getOntologyWords';
+export { default as getQueryResults } from './getQueryResults';
+export type { getQueryResultsProps } from './getQueryResults';
+export { default as getSuggestions } from './getSuggestions';
+export type { getSuggestionProps } from './getSuggestions';
