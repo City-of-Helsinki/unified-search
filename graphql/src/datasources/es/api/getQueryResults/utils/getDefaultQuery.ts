0ab320bf77b2094423516cf2f125df376bbeb8fe
Niko Lindroos <niko.j.makela@gmail.com>
2023-11-21 14:02:27 +0200

    refactor(graphql): handle empty text search variable
    LIIKUNTA-578

--- graphql/src/datasources/es/api/getQueryResults/utils/getDefaultQuery.ts
+++ graphql/src/datasources/es/api/getQueryResults/utils/getDefaultQuery.ts
@@ -1,10 +1,12 @@
+import { GraphQlToElasticLanguageMap } from '../../../../../constants';
 import { escapeQuery } from '../../../../../utils';
+import { ES_DEFAULT_INDEX } from '../../../constants';
 import { searchFieldsBoostMapping } from '../constants';
 import type { GetQueryResultsProps } from '../types';
 
 export function getDefaultQuery({
-  index,
-  languages,
+  index = ES_DEFAULT_INDEX,
+  languages = Object.values(GraphQlToElasticLanguageMap),
   text,
 }: Pick<GetQueryResultsProps, 'index' | 'languages' | 'text'>) {
   // Default query is to search the same thing in every language
@@ -23,7 +25,7 @@ export function getDefaultQuery({
           // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html.
           query_string: {
             // Escape the query string so the special chars are not acting as operators.
-            query: escapeQuery(text),
+            query: text ? escapeQuery(text) : '*',
             // Creates a match_bool_prefix query on each field and combines the _score from each field.
             // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html#type-bool-prefix.
             type: 'bool_prefix',
