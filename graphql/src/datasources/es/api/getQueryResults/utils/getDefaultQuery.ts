d963f70a0d351dade34b4e5c4cdebb72567b642c
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-01 18:21:56 +0200

    feat(graphql): exact matches should be higher in relevance
    US-123
    Use bool-query with `match_bool_prefix` and `match_phrase` instead of `query_string` query.
    
    Add a bool-query to replace the query_string-query,
    since it is simpler and does not need any special care for input.
    
    Add a `match_phrase` query on side of `match_bool_prefix`
    to give higher score for "exact matches".
    
    Add a new query function on besides of the old 
    `query_string` implementation, just to make the future development easier, if we go back to query string.
    
    Example (tested): When searching with a text-parameter set as 
    "Itäkeskuksen uimahalli / Kuntosali",  it returns 
    the exact match as first. The result set is a following 
    (only name fields included):
    1. Itäkeskuksen uimahalli / Kuntosali
    2. Itäkeskuksen uimahalli
    3. Itäkeskuksen uimahalli / Liikuntasali
    4. Itäkeskuksen palvelutalo

--- graphql/src/datasources/es/api/getQueryResults/utils/getDefaultQuery.ts
+++ graphql/src/datasources/es/api/getQueryResults/utils/getDefaultQuery.ts
@@ -1,44 +1,227 @@
 import { GraphQlToElasticLanguageMap } from '../../../../../constants';
+import type { ElasticLanguage, TranslatableField } from '../../../../../types';
 import { escapeQuery } from '../../../../../utils';
 import { ES_DEFAULT_INDEX, SEARCH_ALL_SPECIAL_CHAR } from '../../../constants';
 import { searchFieldsBoostMapping } from '../constants';
-import type { GetQueryResultsProps } from '../types';
+import type { GetQueryResultsProps, QueryFilterClauses } from '../types';
 
 const getQueryString = (text?: string) =>
   text && text !== SEARCH_ALL_SPECIAL_CHAR
     ? escapeQuery(text)
     : SEARCH_ALL_SPECIAL_CHAR;
 
-export function getDefaultQuery({
+type DefaultBoolQueryClausesType =
+  | {
+      match_bool_prefix: {
+        [key in TranslatableField]?: QueryFilterClauses;
+      };
+    }
+  | {
+      match_phrase: {
+        [key in TranslatableField]?: QueryFilterClauses;
+      };
+    };
+
+type DefaultQueryStringQueryClausesType = {
+  query_string: QueryFilterClauses & { fields: TranslatableField[] };
+};
+
+type DefaultBoolQueryClausesAccumulatorType = {
+  [key in ElasticLanguage]?: DefaultBoolQueryClausesType;
+};
+
+type DefaultQueryStringQueryClausesAccumulatorType = {
+  [key in ElasticLanguage]?: DefaultQueryStringQueryClausesType;
+};
+
+/**
+ * @returns An ElasticSearch query object. Example:
+ * ```
+ * { "query": {"bool": { "should": [
+ *  {
+ *    "match_bool_prefix": {
+ *      "venue.description.fi": {
+ *        "query": "Itäkeskuksen uimahalli / Kuntosali",
+ *        "operator": "or",
+ *        "fuzziness": "AUTO",
+ *        "boost": "1"
+ *      }
+ *    }
+ *  },
+ *  {
+ *    "match_bool_prefix": {
+ *      "venue.name.fi": {
+ *        "query": "Itäkeskuksen uimahalli / Kuntosali",
+ *        "operator": "or",
+ *        "fuzziness": "AUTO",
+ *        "boost": "3"
+ *      }
+ *    }
+ *  },
+ *  {
+ *    "match_phrase": {
+ *      "venue.description.fi": {
+ *        "query": "Itäkeskuksen uimahalli / Kuntosali",
+ *        "boost": 2
+ *      }
+ *    }
+ *  },
+ *  {
+ *    "match_phrase": {
+ *      "venue.name.fi": {
+ *        "query": "Itäkeskuksen uimahalli / Kuntosali",
+ *        "boost": 6
+ *      }
+ *    }
+ *  }
+ * ]}}}
+ * ```
+ * */
+export function getDefaultBoolQuery({
   index = ES_DEFAULT_INDEX,
   languages = Object.values(GraphQlToElasticLanguageMap),
   text,
 }: Pick<GetQueryResultsProps, 'index' | 'languages' | 'text'>) {
-  // Default query is to search the same thing in every language
-  return languages.reduce(
-    (acc, language) => ({
-      ...acc,
-      [language]: Object.entries(searchFieldsBoostMapping)
-        // Don't map empty field sets to query
-        .filter(([, searchFields]) => searchFields(language, index).length)
-        .map(([boost, searchFields]) => ({
-          // You can use the `query_string` query to create a complex search
-          // that includes wildcard characters,
-          // searches across multiple fields, and more.
-          // While versatile, the query is strict and
-          // returns an error if the query string includes any invalid syntax.
-          // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html.
-          query_string: {
-            // Escape the query string so the special chars are not acting as operators.
-            query: getQueryString(text),
-            // Creates a match_bool_prefix query on each field and combines the _score from each field.
-            // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html#type-bool-prefix.
-            type: 'bool_prefix',
-            boost,
-            fields: searchFields(language, index),
-          },
-        })),
-    }),
-    {}
-  );
+  const should = Object.values(
+    languages.reduce(
+      (acc: DefaultBoolQueryClausesAccumulatorType, language) => ({
+        ...acc,
+        [language]: [
+          ...Object.entries(searchFieldsBoostMapping).map(
+            ([boost, searchFields]) => ({
+              match_bool_prefix: {
+                ...searchFields(language, index).reduce<{
+                  [key in TranslatableField]?: QueryFilterClauses;
+                }>(
+                  (queries, queryField) => ({
+                    ...queries,
+                    [queryField]: {
+                      query: text,
+                      operator: 'or',
+                      fuzziness: 'AUTO',
+                      boost,
+                    },
+                  }),
+                  {}
+                ),
+              },
+            })
+          ),
+          ...Object.entries(searchFieldsBoostMapping).map(
+            ([boost, searchFields]) => ({
+              match_phrase: {
+                ...searchFields(language, index).reduce<{
+                  [key in TranslatableField]?: QueryFilterClauses;
+                }>(
+                  (queries, queryField) => ({
+                    ...queries,
+                    [queryField]: {
+                      query: text,
+                      boost: parseInt(boost) * 2,
+                    },
+                  }),
+                  {}
+                ),
+              },
+            })
+          ),
+        ],
+      }),
+      {}
+    )
+  ).flat();
+  return {
+    query: {
+      bool: {
+        should,
+      },
+    },
+  };
+}
+
+/**
+ * @deprecated Use getDefaultBoolQuery instead.
+ * @returns An ElasticSearch query object. Example:
+ * ```
+ * { "query": { "bool": { "should": [
+ *   {
+ *     "query_string": {
+ *       "query": "Itäkeskuksen uimahalli \\\\/ Kuntosali",
+ *       "type": "bool_prefix",
+ *       "boost": "1",
+ *       "fields": ["venue.description.fi"]
+ *     }
+ *   },
+ *   {
+ *     "query_string": {
+ *       "query": "Itäkeskuksen uimahalli \\\\/ Kuntosali",
+ *       "type": "bool_prefix",
+ *       "boost": "3",
+ *       "fields": ["venue.name.fi"]
+ *     }
+ *   }
+ * ]}}}
+ * ```
+ */
+export function getDefaultQueryStringQuery({
+  index = ES_DEFAULT_INDEX,
+  languages = Object.values(GraphQlToElasticLanguageMap),
+  text,
+}: Pick<GetQueryResultsProps, 'index' | 'languages' | 'text'>) {
+  const should = Object.values(
+    languages.reduce(
+      (acc: DefaultQueryStringQueryClausesAccumulatorType, language) => ({
+        ...acc,
+        [language]: Object.entries(searchFieldsBoostMapping)
+          // Don't map empty field sets to query
+          .filter(([, searchFields]) => searchFields(language, index).length)
+          .map(([boost, searchFields]) => ({
+            // You can use the `query_string` query to create a complex search
+            // that includes wildcard characters,
+            // searches across multiple fields, and more.
+            // While versatile, the query is strict and
+            // returns an error if the query string includes any invalid syntax.
+            // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html.
+            query_string: {
+              // Escape the query string so the special chars are not acting as operators.
+              query: getQueryString(text),
+              // Creates a match_bool_prefix query on each field and combines the _score from each field.
+              // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html#type-bool-prefix.
+              type: 'bool_prefix',
+              boost,
+              fields: searchFields(language, index),
+            },
+          })),
+      }),
+      {}
+    )
+  ).flat();
+  return {
+    query: {
+      bool: {
+        should,
+      },
+    },
+  };
+}
+
+export enum QueryType {
+  MatchBoolPrefix = 'match_bool_prefix',
+  QueryString = 'query_string',
+}
+
+const defaultQueryForType: Record<
+  QueryType,
+  ({
+    index,
+    languages,
+    text,
+  }: Pick<GetQueryResultsProps, 'index' | 'languages' | 'text'>) => any
+> = {
+  [QueryType.MatchBoolPrefix]: getDefaultBoolQuery,
+  [QueryType.QueryString]: getDefaultQueryStringQuery,
+};
+
+export function getDefaultQueryForQueryType(type: QueryType) {
+  return defaultQueryForType[type];
 }
