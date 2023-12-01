f354ab590543dd014b341058aef250089eb3e025
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-01 18:24:19 +0200

    refactor(graphql): remove needless query_string query
    US-123.
    There is no need anymore for the query_string query and it's
    types and utilities, so they can be removed.
    
    NOTE: The remove is now in it's own commit, so it's easy to be reverted.

--- graphql/src/datasources/es/api/getQueryResults/utils/getDefaultQuery.ts
+++ graphql/src/datasources/es/api/getQueryResults/utils/getDefaultQuery.ts
@@ -1,14 +1,8 @@
 import { GraphQlToElasticLanguageMap } from '../../../../../constants';
 import type { ElasticLanguage, TranslatableField } from '../../../../../types';
-import { escapeQuery } from '../../../../../utils';
-import { ES_DEFAULT_INDEX, SEARCH_ALL_SPECIAL_CHAR } from '../../../constants';
+import { ES_DEFAULT_INDEX } from '../../../constants';
 import { searchFieldsBoostMapping } from '../constants';
 import type { GetQueryResultsProps, QueryFilterClauses } from '../types';
-
-const getQueryString = (text?: string) =>
-  text && text !== SEARCH_ALL_SPECIAL_CHAR
-    ? escapeQuery(text)
-    : SEARCH_ALL_SPECIAL_CHAR;
 
 type DefaultBoolQueryClausesType =
   | {
@@ -22,16 +16,8 @@ type DefaultBoolQueryClausesType =
       };
     };
 
-type DefaultQueryStringQueryClausesType = {
-  query_string: QueryFilterClauses & { fields: TranslatableField[] };
-};
-
 type DefaultBoolQueryClausesAccumulatorType = {
   [key in ElasticLanguage]?: DefaultBoolQueryClausesType;
-};
-
-type DefaultQueryStringQueryClausesAccumulatorType = {
-  [key in ElasticLanguage]?: DefaultQueryStringQueryClausesType;
 };
 
 /**
@@ -137,91 +123,4 @@ export function getDefaultBoolQuery({
       },
     },
   };
-}
-
-/**
- * @deprecated Use getDefaultBoolQuery instead.
- * @returns An ElasticSearch query object. Example:
- * ```
- * { "query": { "bool": { "should": [
- *   {
- *     "query_string": {
- *       "query": "Itäkeskuksen uimahalli \\\\/ Kuntosali",
- *       "type": "bool_prefix",
- *       "boost": "1",
- *       "fields": ["venue.description.fi"]
- *     }
- *   },
- *   {
- *     "query_string": {
- *       "query": "Itäkeskuksen uimahalli \\\\/ Kuntosali",
- *       "type": "bool_prefix",
- *       "boost": "3",
- *       "fields": ["venue.name.fi"]
- *     }
- *   }
- * ]}}}
- * ```
- */
-export function getDefaultQueryStringQuery({
-  index = ES_DEFAULT_INDEX,
-  languages = Object.values(GraphQlToElasticLanguageMap),
-  text,
-}: Pick<GetQueryResultsProps, 'index' | 'languages' | 'text'>) {
-  const should = Object.values(
-    languages.reduce(
-      (acc: DefaultQueryStringQueryClausesAccumulatorType, language) => ({
-        ...acc,
-        [language]: Object.entries(searchFieldsBoostMapping)
-          // Don't map empty field sets to query
-          .filter(([, searchFields]) => searchFields(language, index).length)
-          .map(([boost, searchFields]) => ({
-            // You can use the `query_string` query to create a complex search
-            // that includes wildcard characters,
-            // searches across multiple fields, and more.
-            // While versatile, the query is strict and
-            // returns an error if the query string includes any invalid syntax.
-            // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html.
-            query_string: {
-              // Escape the query string so the special chars are not acting as operators.
-              query: getQueryString(text),
-              // Creates a match_bool_prefix query on each field and combines the _score from each field.
-              // See: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html#type-bool-prefix.
-              type: 'bool_prefix',
-              boost,
-              fields: searchFields(language, index),
-            },
-          })),
-      }),
-      {}
-    )
-  ).flat();
-  return {
-    query: {
-      bool: {
-        should,
-      },
-    },
-  };
-}
-
-export enum QueryType {
-  MatchBoolPrefix = 'match_bool_prefix',
-  QueryString = 'query_string',
-}
-
-const defaultQueryForType: Record<
-  QueryType,
-  ({
-    index,
-    languages,
-    text,
-  }: Pick<GetQueryResultsProps, 'index' | 'languages' | 'text'>) => any
-> = {
-  [QueryType.MatchBoolPrefix]: getDefaultBoolQuery,
-  [QueryType.QueryString]: getDefaultQueryStringQuery,
-};
-
-export function getDefaultQueryForQueryType(type: QueryType) {
-  return defaultQueryForType[type];
 }
