6d7a4824d812e9978df50779352bdd36286cf28e
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-04 13:08:00 +0200

    feat(graphql): include ontologies in default search query
    US-124.
    Search also from the ontologies when using
    the default text search query for locations.

--- graphql/src/datasources/es/api/getQueryResults/utils/getOntologyQuery.ts
+++ graphql/src/datasources/es/api/getQueryResults/utils/getOntologyQuery.ts
@@ -1,7 +1,33 @@
 import { GraphQlToElasticLanguageMap } from '../../../../../constants';
+import { type ElasticLanguage } from '../../../../../types';
 import { ES_DEFAULT_INDEX } from '../../../constants';
 import type { GetQueryResultsProps } from '../types';
 import { getOntologyFields } from './getOntologyFields';
+
+export const getOntologyMatchers = ({
+  index = ES_DEFAULT_INDEX,
+  languages = Object.values(GraphQlToElasticLanguageMap),
+  query,
+}: Pick<GetQueryResultsProps, 'index' | 'languages'> & { query: string }) =>
+  languages.reduce<{
+    [key in ElasticLanguage]?: {
+      multi_match: {
+        query: string;
+        fields: ReturnType<typeof getOntologyFields>;
+      };
+    };
+  }>(
+    (acc, language) => ({
+      ...acc,
+      [language]: {
+        multi_match: {
+          query,
+          fields: getOntologyFields(language, index),
+        },
+      },
+    }),
+    {}
+  );
 
 /** @deprecated Deprecated as unused. */
 export function getOntologyQuery({
@@ -9,18 +35,11 @@ export function getOntologyQuery({
   languages = Object.values(GraphQlToElasticLanguageMap),
   ontology,
 }: Pick<GetQueryResultsProps, 'index' | 'languages' | 'ontology'>) {
-  const ontologyMatchers = languages.reduce(
-    (acc, language) => ({
-      ...acc,
-      [language]: {
-        multi_match: {
-          query: ontology,
-          fields: getOntologyFields(language, index),
-        },
-      },
-    }),
-    {}
-  );
+  const ontologyMatchers = getOntologyMatchers({
+    index,
+    languages,
+    query: ontology,
+  });
 
   return {
     query: {
