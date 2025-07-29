9858f57f72bd8a08e4d12bab3a0890f275c1549f
Kari Salminen <kari.salminen@anders.com>
2025-07-29 14:35:41 +0300

    feat(graphql): use better workaround for readiness build time
    Similarly as in sources app's settings.py a file's modification time is
    used as a workaround for approximating readiness endpoint's build time.
    This is not foolproof, but better than nothing—the previous way of using
    the file's evaluation time was probably more problematic than this as it
    probably would've changed if the pod got respawned.
    
    refs US-151

--- graphql/src/readiness.ts
+++ graphql/src/readiness.ts
@@ -1,15 +1,20 @@
+import { statSync } from 'fs';
+import { fileURLToPath } from 'url';
+
 import type { Response } from 'express';
 
 import packageJson from '../package.json' with { type: 'json' };
 
-// Use this file's evaluation time as a fallback value for build time
-const THIS_FILE_EVALUATION_TIME = new Date().toISOString();
+// Use this file's modification time as a workaround for app build time
+const THIS_FILE_MODIFICATION_TIME = new Date(
+  statSync(fileURLToPath(import.meta.url)).mtime
+).toISOString();
 
 export default function readiness(response: Response) {
   const release = process.env.APP_RELEASE ?? '';
   const packageVersion = packageJson.version;
   const commitHash = process.env.APP_COMMIT_HASH ?? '';
-  const buildTime = process.env.APP_BUILD_TIME ?? THIS_FILE_EVALUATION_TIME;
+  const buildTime = process.env.APP_BUILD_TIME ?? THIS_FILE_MODIFICATION_TIME;
 
   response.status(200).json({
     status: 'ok',
