4a8a57daf7c669873aafb602ac2b24c5c919a2c1
Kari Salminen <kari.salminen@anders.com>
2025-08-22 14:29:13 +0300

    fix(sources): make request_json function retry request twice
    This affects at least the following importers:
     - location
       - Including fetching of the opening hours from hauki.api.hel.fi
     - ontology_word
     - ontology_tree
    
    refs US-139

--- sources/ingest/importers/utils/retry.py
+++ sources/ingest/importers/utils/retry.py
@@ -4,10 +4,12 @@ import time
 def retry_twice_5s_intervals(callable, *args, **kwargs):
     """
     Retry given callable (e.g. function or class) with given arguments
-    twice with 5s intervals (i.e. max. 3 tries = max. 2 retries).
+    twice with 5s intervals (i.e. max. 3 tries = max. 2 retries)
+    if it raises an exception, otherwise return first successful result.
 
-    :return: The first successful result is returned, or the last
-             exception is raised if all attempts fail.
+    :return: The first successful result is returned
+             (i.e. the value of `callable(*args, **kwargs)`),
+             or the last exception is raised if all attempts fail.
 
     :param callable: The callable to be retried.
     :param args: Positional arguments to pass to the callable.
--- sources/ingest/importers/utils/traffic.py
+++ sources/ingest/importers/utils/traffic.py
@@ -2,17 +2,30 @@ import logging
 
 import requests
 
+from ingest.importers.utils.retry import retry_twice_5s_intervals
+
 logger = logging.getLogger(__name__)
 
 
 def request_json(url, timeout_seconds=20):
+    """
+    Request a JSON response from the given URL
+    with the given timeout and max. 2 retries.
+
+    :return: JSON response from the URL if successful,
+             otherwise raises an exception.
+    :raise: Exception if the request fails after retries.
+    """
     logger.debug(f"Requesting URL {url}")
-    result = None
+
+    def fetch_response():
+        response = requests.get(url, timeout=timeout_seconds)
+        response.raise_for_status()
+        return response
+
     try:
-        r = requests.get(url, timeout=timeout_seconds)
-        r.raise_for_status()
-        result = r.json()
+        response = retry_twice_5s_intervals(fetch_response)
+        return response.json()
     except Exception as e:
         logger.error(f"Error while requesting {url}: {e}")
         raise
-    return result
