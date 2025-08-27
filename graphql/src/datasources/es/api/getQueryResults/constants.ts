8ce9705fa37ce4acbb7ae26f8f404ca06a61c210
Kari Salminen <kari.salminen@anders.com>
2025-08-27 10:44:09 +0300

    test(graphql): test search weights & fix boost values to be floats
    Search weights had been sometimes handled as strings and sometimes as
    integers, this tries to make their handling to be more consistently as
    floats.
    
    refs US-171 (add to test coverage & correctness)

--- graphql/src/datasources/es/api/getQueryResults/constants.ts
+++ graphql/src/datasources/es/api/getQueryResults/constants.ts
@@ -15,6 +15,12 @@ import type { ElasticSearchIndex } from '../../types.js';
  * normal < high < veryHigh (i.e. they are ordered)
  * and that they're different enough to have an effect on the search result
  * (otherwise they would be meaningless).
+ *
+ * The values could be changed to non-integers if more granularity is needed,
+ * but please check tests and functionality if you do that, as handling
+ * floating point values has its pitfalls. As a recommendation, keep to
+ * exactly representable floating point numbers i.e. fractions that are sums of
+ * powers of two, e.g. 1.5=2^0+2^-1 or 2.125=2^1+2^-3, to avoid precision issues.
  */
 export const SEARCH_WEIGHT = {
   normal: 1,
@@ -22,9 +28,23 @@ export const SEARCH_WEIGHT = {
   veryHigh: 3,
 } as const;
 
+/**
+ * Boost multiplier for `match_phrase` queries.
+ *
+ * Meant to make sure that exact phrase matches are ranked higher than
+ * regular matches.
+ *
+ * For example, if a user searches for "art museum" we want to make sure
+ * that results containing the exact phrase "art museum" are ranked higher
+ * than those that just contain the words "art" and "museum" separately.
+ *
+ * Must be greater than 1 to have an effect.
+ */
+export const MATCH_PHRASE_BOOST_MULTIPLIER = SEARCH_WEIGHT.high;
+
 // Some fields should be boosted / weighted to get more relevant result set
 export const searchFieldsBoostMapping: Record<
-  string | number,
+  string,
   (lang: ElasticLanguage, index: ElasticSearchIndex) => TranslatableField[]
 > = {
   // Normally weighted search fields for different indexes
