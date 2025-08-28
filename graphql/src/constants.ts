559c78b150f6b99f3b5f86d07637672802c2e788
Kari Salminen <kari.salminen@anders.com>
2025-08-28 14:05:45 +0300

    refactor(graphql): move corsConfig to constants file
    refs US-171 (trying to make graphql/src/index.ts more manageable)

--- graphql/src/constants.ts
+++ graphql/src/constants.ts
@@ -1,3 +1,5 @@
+import type { CorsOptions } from 'cors';
+
 export const GraphQlToElasticLanguageMap = {
   FINNISH: 'fi',
   SWEDISH: 'sv',
@@ -11,6 +13,36 @@ export const GraphQlToElasticLanguageMap = {
  */
 export const elasticSearchQueryStringSpecialCharsRegExpPattern =
   /(\+|-|=|&&|\|\||>|<|!|\(|\)|\{|\}|\[|\]|\^|"|~|\*|\?|:|\\|\/)/gi;
+
+/**
+ * CORS (Cross-Origin Resource Sharing) configuration for the GraphQL server.
+ *
+ * Configuration options documented at:
+ * https://github.com/expressjs/cors/tree/v2.8.5?tab=readme-ov-file#configuration-options
+ */
+export const corsConfig = {
+  // Allow all origins because:
+  // 1. This is a public API
+  // 2. No authentication is used
+  // 3. No authorization is used
+  origin: '*',
+  // Don't allow credentials to be sent with requests
+  // because this a public API and no authentication is used.
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Access-Control-Allow-Credentials
+  credentials: false,
+  // Allow methods:
+  // - OPTIONS for preflight requests
+  // - POST for GraphQL queries and mutations
+  methods: ['OPTIONS', 'POST'],
+  // Allow CORS-safelisted request headers
+  // Accept, Accept-Language, Content-Language, Content-Type and Range
+  // that are always allowed and bypass the restrictions on Content-Type header
+  // to allow application/json content type.
+  //
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Access-Control-Allow-Headers
+  // https://developer.mozilla.org/en-US/docs/Glossary/CORS-safelisted_request_header
+  allowedHeaders: ['Content-Type'],
+} as const satisfies CorsOptions;
 
 // Content Security Policy (CSP) keywords
 // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CSP
--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -9,12 +9,12 @@ import responseCachePlugin from '@apollo/server-plugin-response-cache';
 import { buildSubgraphSchema } from '@apollo/subgraph';
 import { expressMiddleware } from '@as-integrations/express5';
 import * as Sentry from '@sentry/node';
-import cors, { CorsOptions } from 'cors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import helmet, { HelmetOptions } from 'helmet';
 
-import { CSP } from './constants.js';
+import { corsConfig, CSP } from './constants.js';
 import { ElasticSearchAPI } from './datasources/es/index.js';
 import type {
   AccessibilityProfileType,
@@ -77,36 +77,6 @@ function getLandingPagePlugin() {
   }
   return ApolloServerPluginLandingPageDisabled();
 }
-
-/**
- * CORS (Cross-Origin Resource Sharing) configuration for the GraphQL server.
- *
- * Configuration options documented at:
- * https://github.com/expressjs/cors/tree/v2.8.5?tab=readme-ov-file#configuration-options
- */
-const corsConfig = {
-  // Allow all origins because:
-  // 1. This is a public API
-  // 2. No authentication is used
-  // 3. No authorization is used
-  origin: '*',
-  // Don't allow credentials to be sent with requests
-  // because this a public API and no authentication is used.
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Access-Control-Allow-Credentials
-  credentials: false,
-  // Allow methods:
-  // - OPTIONS for preflight requests
-  // - POST for GraphQL queries and mutations
-  methods: ['OPTIONS', 'POST'],
-  // Allow CORS-safelisted request headers
-  // Accept, Accept-Language, Content-Language, Content-Type and Range
-  // that are always allowed and bypass the restrictions on Content-Type header
-  // to allow application/json content type.
-  //
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Access-Control-Allow-Headers
-  // https://developer.mozilla.org/en-US/docs/Glossary/CORS-safelisted_request_header
-  allowedHeaders: ['Content-Type'],
-} as const satisfies CorsOptions;
 
 /**
  * CSP (i.e. Content Security Policy) configuration
